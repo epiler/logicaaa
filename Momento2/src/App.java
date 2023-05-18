767740c2305f65b4a456bf9de401057fc90f2fad
epiler <130491817+epiler@users.noreply.github.com>
2023-05-17 21:32:47 -0500

    Add files via upload

---
+++ Momento2/src/App.java
@@ -0,0 +1,137 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+
+
+    /*1.	Realizar un algoritmo que lea las cinco calificaciones obtenidas por el estudiante y obtenga su promedio el cual equivale al 70% de la nota del periodo de la asignatura de redes de datos. La nota del examen final del periodo equivale al 20% y la autoevaluación al 10%. Obtener la nota definitiva del estudiante e indique el nivel de desempeño del estudiante. Tenga en cuenta que la escala de calificación es de 1,0 a 5,0 y los niveles de desempeño vienen determinados por la información suministrada en la tabla: 1.nota 20% 2.10 y 70%*/
+
+    /*2.	Realizar un algoritmo que lea 5 números y solamente imprima los positivos. Recuerden que los números son positivos cuando son mayores que cero. */
+
+    /*Scanner leer = new Scanner(System.in);
+
+    System.out.println("ingrese 5 numeros positivos");
+    
+    
+    int num1, num2,num3,num4,num5;
+
+    System.out.println("Ingrese el primer numero");
+    num1 = leer.nextInt();
+
+    System.out.println("Ingrese el segundo numero");
+    num2 = leer.nextInt();
+
+    System.out.println("Ingrese el tercer numero");
+    num3 = leer.nextInt();
+
+    System.out.println("Ingrese el cuarto numero ");
+    num4 = leer.nextInt();
+
+    System.out.println("Ingrese el quinto numero ");
+    num5 = leer.nextInt();
+
+    for (int i = 0; i < 5; i++) {
+        
+    }if (num1 > 0) {
+        System.out.println(num1);
+    }if (num2 > 0) {
+        System.out.println(num2);
+    }if (num3 > 0) {
+        System.out.println(num3);
+    }if (num4 > 0) {
+        System.out.println(num4);
+    }if (num5 > 0) {
+        System.out.println(num5);
+    }else{
+        System.out.println( );
+    }*/
+    
+        
+       
+    /*
+3.	Realizar un algoritmo que imprima la tabla de multiplicar del 2 de la siguiente manera.
+ */
+    /*int resultado = 0;
+    int multiplicador = 2;
+
+    System.out.println("***Ejecución Iniciada***");
+
+    for (int i = 1; i <= 10 ; i++) {
+
+        resultado = i * multiplicador ;
+        
+        System.out.println(i + " * " + "2 " + " = " + resultado);
+    
+    }
+    System.out.println("***Ejecución Finalizada***");*/
+
+    /*4.	Crea una aplicación que nos pida un día de la semana y que nos diga si es un día laboral o no. Usa un switch para ello. */
+
+    /*Scanner leer = new Scanner(System.in);
+
+    String dias;
+
+    System.out.println("Ingrese un dia de la semana lunes, martes, miercoles, jueves, viernes, sabado, domingo");
+    dias = leer.nextLine();
+
+    switch (dias) {
+        case "lunes" :
+        case "martes":
+        case "miercoles":
+        case "jueves":
+        case "viernes":
+    
+        System.out.println("Es dia laboral");
+            break;
+    
+        default:
+        case "sabado":
+        case "domingo":
+        System.out.println("No es dia laboral");
+            break;
+            
+    } */
+            
+            
+
+
+        /* 5.	Escribe una aplicación que contenga una contraseña cualquiera n numérica. Después se solicitará que introduzcas la contraseña, el usuario solo tiene con 3 intentos. Cuando aciertes ya no pedirá más la contraseña y mostrará un mensaje diciendo «Bienvenido al sistema». */
+
+        /*Scanner leer = new Scanner(System.in);
+
+        String password = "hola";
+
+        
+
+        int intentos = 3;
+
+        
+
+        while (intentos > 0) {
+            System.out.println("Introduce la contraseña");
+            String ingreso = leer.nextLine();
+            
+
+            if (ingreso.equals(password)) {
+                System.out.println("Bienvenido al sistema");
+                break;
+            }else {
+                intentos--;
+                System.out.println("contraseña incorrecta, le quedan  " + intentos  );
+            }
+        }
+        if (intentos == 0) {
+            System.out.println("Acceso denegado");
+        }*/
+
+        /*2.	Realizar un algoritmo que lea 5 números y solamente imprima los positivos. Recuerden que los números son positivos cuando son mayores que cero. */
+
+
+        
+        
+
+
+    }
+}
+
+
